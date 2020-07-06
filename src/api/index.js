d5c05c49ded775b97b1e5050df977207c801cf21
Siddhesh Rao <47262015+isiddheshrao@users.noreply.github.com>
2020-07-06 18:02:01 -0500

    Chart and Card Layout with API

--- src/api/index.js
+++ src/api/index.js
@@ -7,10 +7,26 @@ const url = 'https://covid19.mathdro.id/api';
 
 export const fetchData = async () => {
     try{
-        const response = await axios.get(url);
-        return response;
-        
+        const { data: {confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+
+        return {confirmed, recovered, deaths, lastUpdate };
+
     } catch (error) {
 
     }
-}
+};
+
+export const dailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }));
+        return modifiedData;
+    }catch (error){
+
+    }
+};
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { dailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
+    const [currentDailyData, setDailyData] = useState([]);
+
+    useEffect(() =>{
+        const fetchAPI = async () =>{
+            setDailyData(await dailyData());
+        }
+        fetchAPI();
+    });
+
+    // creating main line chart
+    const lineChart = (
+        currentDailyData.length
+        ?(
+        <Line
+            data = {{
+                labels: currentDailyData.map(({date}) => date),
+                datasets: [{
+                    data: currentDailyData.map(({ confirmed }) => confirmed),
+                    label: 'Infected',
+                    borderColor: '#3333ff',
+                    fill: true,
+                },{
+                    data: currentDailyData.map(({ deaths }) => deaths),
+                    label: 'Deaths',
+                    borderColor: 'red',
+                    backgroundColor: 'rgba(255,0,0,0.5)',
+                    fill: true,
+                }],
+            }}
+        />): null
+    )
+
     return(
-        <h1>Chart</h1>
+        <div className={styles.container}>
+            {lineChart}
+        </div>
     )
 }
 
